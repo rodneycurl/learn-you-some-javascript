1b0848fce73acced9e2bd74eae93e2dca816199b
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-10-17 11:43:39 -0400

    initial commit

---
+++ learnyounode/make-it-modular.js
@@ -0,0 +1,9 @@
+let lslib = require('./lslib');
+let dirname = process.argv[2];
+let ext = process.argv[3];
+
+lslib(dirname, ext, function(err, files) {
+  for (i = 0; i < files.length; i++) {
+    console.log(files[i]);
+  }
+});
