3d19c221e5ba521fcb33185fc75a40e0fd12f0f1
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-10-19 15:56:49 -0400

    updating solution for 11-implement-map-with-reduce.js

--- functional-javascript/11-implement-map-with-reduce.js
+++ functional-javascript/11-implement-map-with-reduce.js
@@ -1,5 +1,24 @@
+// second attempt
 module.exports = function arrayMap(arr, fn) {
-  return arr.reduce(function(acc, item, index, arr) {
-    return acc.concat(fn(item, index, arr))
-  }, [])
+  var result = [];
+  arr.reduce(function(prev, curr, index, arr) {
+  	result.push(fn(curr));
+  }, null);
+  return result;
 }
+
+// initial attempt
+// module.exports = function arrayMap(arr, fn) {
+//   return arr.reduce(function(acc, item, index, arr) {
+//     return acc.concat(fn(item, index, arr))
+//   }, [])
+// }
+
+// official solution
+//
+// module.exports = function arrayMap(arr, fn, thisArg) {
+//   return arr.reduce(function(acc, item, index, arr) {
+//     acc.push(fn.call(thisArg, item, index, arr))
+//     return acc
+//   }, [])
+// }
